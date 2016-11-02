2bab8a115f096732a2ec844b53a224597be499ed
P0K3SCP <bblopes99@gmail.com>
2016-11-02 21:04:26 +0000

    Add files via upload

---
+++ Encriptacao.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Introduza o que quer encriptar\n\n");
+
+// Parte que vai fazer a leitura da string
+
+
+
+// Fim da parte que vai fazer a leitura da string
+
+    return 0;
+}
