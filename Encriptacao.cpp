89216f003560fd6d9536ace7162222a33cf8fd83
P0K3SCP <bblopes99@gmail.com>
2016-11-02 21:10:34 +0000

    Update Encriptacao.cpp

--- Encriptacao.cpp
+++ Encriptacao.cpp
@@ -2,11 +2,18 @@
 
 int main()
 {
+    
+// Tabela de encriptacao
+    
+    char encriptacao[256] = {}
+    
+// Fim da tabela de encriptacao
+    
     printf("Introduza o que quer encriptar\n\n");
 
 // Parte que vai fazer a leitura da string
 
-
+    
 
 // Fim da parte que vai fazer a leitura da string
 
