879a2354a5fe570bd935591207ca707e76eb6729
P0K3SCP <bblopes99@gmail.com>
2016-11-02 21:42:25 +0000

    Update Encriptacao.cpp

--- Encriptacao.cpp
+++ Encriptacao.cpp
@@ -2,20 +2,20 @@
 
 int main()
 {
-    
-// Tabela de encriptacao
-    
-    char encriptacao[256] = {}
-    
-// Fim da tabela de encriptacao
-    
+
+    // Tabela de encriptacao
+
+    char encriptacao[][256] = {" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "X", "Y", "W", "Z", "a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "x", "y", "w", "z"};
+
+    // Fim da tabela de encriptacao
+
     printf("Introduza o que quer encriptar\n\n");
 
-// Parte que vai fazer a leitura da string
+    // Parte que vai fazer a leitura da string
 
-    
 
-// Fim da parte que vai fazer a leitura da string
+
+    // Fim da parte que vai fazer a leitura da string
 
     return 0;
 }
